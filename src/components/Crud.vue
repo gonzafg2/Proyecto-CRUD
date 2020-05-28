2df18bfb1ad715b4887392e086030e52b133bcaf
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-28 12:45:44 -0400

    Agrego campos de correo y clave

--- src/components/Crud.vue
+++ src/components/Crud.vue
@@ -1,12 +1,43 @@
 <template>
-  <div></div>
+  <div class="container" id="crud">
+    <form action="" class="">
+      <div class="form-group">
+        <label for="correo">Correo Electrónico</label>
+        <input
+          type="email"
+          name=""
+          id="correoId"
+          class="form-control"
+          placeholder="Escriba su correo electrónico"
+          v-model="correo"
+        />
+      </div>
+      <div class="form-group">
+        <label for="correo">Contraseña</label>
+        <input
+          type="text"
+          name=""
+          id="claveId"
+          class="form-control"
+          placeholder="Escriba su contraseña"
+          v-model="clave"
+        />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      correo: "",
+      clave: ""
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Crud></Crud>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Crud from "@/components/Crud.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Crud
   }
 };
 </script>
